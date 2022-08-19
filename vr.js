0c5c6c44d8ab34f4d5ea7631125fd4c7b890f762
ahmad199617 <95858731+ahmad199617@users.noreply.github.com>
2022-08-19 09:51:06 +0300

    VR

---
+++ vr.js
@@ -0,0 +1,20 @@
+
+
+
+
+window.addEventListener('load', function () {
+   
+    
+    setTimeout(showPagex, 2000);
+    setTimeout(showPage, 2500);
+    
+    
+    
+  })
+
+    function showPage() {
+    document.getElementById("LoadingID").style.opacity = "0%";}
+    
+    function showPagex() {
+    document.getElementById("LoadingID").style.display = "none";}  
+
